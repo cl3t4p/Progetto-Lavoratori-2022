60cdc0efc484906d25f2c88405342f1b9696517d
cl3t4p <cl3t4p@gmail.com>
2022-07-25 12:22:00 +0200

    Added Enter for login

--- src/main/java/com/cl3t4p/progetto/lavoratori2022/fx/controllers/main/LoginController.java
+++ src/main/java/com/cl3t4p/progetto/lavoratori2022/fx/controllers/main/LoginController.java
@@ -2,12 +2,15 @@ package com.cl3t4p.progetto.lavoratori2022.fx.controllers.main;
 
 import com.cl3t4p.progetto.lavoratori2022.Main;
 import com.cl3t4p.progetto.lavoratori2022.fx.JavaFXError;
+import com.cl3t4p.progetto.lavoratori2022.repo.DataRepo;
 import com.cl3t4p.progetto.lavoratori2022.repo.DipendenteRepo;
 import com.cl3t4p.progetto.lavoratori2022.type.Dipendente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 
@@ -15,6 +18,7 @@ import lombok.experimental.FieldDefaults;
 public class LoginController {
 
     final DipendenteRepo dipendenteRepo = Main.getRepo().getDipendenteRepo();
+    final DataRepo dataRepo = Main.getDataRepo();
 
     @FXML
     TextField txuser;
@@ -27,9 +31,15 @@ public class LoginController {
         String password = txpass.getText();
         Dipendente dipendente = dipendenteRepo.getDipendenteByUserAndPassword(username, password);
         if (dipendente != null) {
-            Main.getDataRepo().setDipendente(dipendente);
+            dataRepo.setDipendente(dipendente);
             Main.getLoader().loadView("MENU");
         } else
             JavaFXError.show("Username o password errati!");
+    }
+
+    @FXML
+    private void enterPress(KeyEvent event) {
+        if(event.getCode().equals(KeyCode.ENTER))
+            login(new ActionEvent());
     }
 }
